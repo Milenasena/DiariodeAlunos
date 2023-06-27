9c60bd1ee0e4f543801b01db1e2e015e59ad445b
joaozeta <melodelimajoaovictor@gmail.com>
2023-06-27 11:22:07 -0300

    Initial 66

--- app/src/main/java/com/example/diriodeclasse/MainActivity.kt
+++ app/src/main/java/com/example/diriodeclasse/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -54,7 +55,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CartaoAluno(aluno:Aluno) {
-    var expandir by remeber
+    var expandir by remember { mutableSetOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
