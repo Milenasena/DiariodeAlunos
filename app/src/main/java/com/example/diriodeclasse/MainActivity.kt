8a3ba0f6b966608906c3a5957b6811c41bb77214
joaozeta <melodelimajoaovictor@gmail.com>
2023-06-27 10:52:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/diriodeclasse/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.diriodeclasse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.node.modifierElementOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.diriodeclasse.ui.theme.DiárioDeClasseTheme
+import model.Aluno
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiárioDeClasseTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CartaoAluno(aluno:Aluno) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(160.dp)
+            .padding(15.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(7.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                
+                Image(
+                    painter = painterResource(id =aluno.foto),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(RoundedCornerShape(11.dp))
+                )
+                
+                Column() {
+                 Text(text = aluno.nome)
+                 Text(text =  aluno.curso)
+
+                }
+                Spacer(modifier = Modifier.width(70.dp))
+                //Ele expande as notificações no celular
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowUp,
+                    contentDescription = null
+                )
+
+            }
+            Spacer(modifier = Modifier.width(70))
+            //to string valor em inteiro
+            Text(text = aluno.faltas.toString()
+            )
+            Text(text = aluno.nota.toString()
+            )
+        }
+    }
+}
+@Preview
+@Composable
+fun PreviwCardAluno(){
+    CartaoAluno(
+        aluno = )
+}
+
+    
+
---
+++ app/src/main/java/model/Aluno.kt
@@ -0,0 +1,14 @@
+package model
+
+import com.example.diriodeclasse.R
+
+class Aluno (
+     val nome: String,
+     val curso: String
+
+     ){
+    var foto: Int = R.drawable.i1
+    var faltas:Int= 0
+    var nota: Int= 0
+
+}
