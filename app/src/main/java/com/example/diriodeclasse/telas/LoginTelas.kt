b75524321c18fc19ce3b72a017c0f666bc832e09
joaozeta <melodelimajoaovictor@gmail.com>
2023-06-29 11:17:05 -0300

    Merge remote-tracking branch 'github/master'

--- app/src/main/java/com/example/diriodeclasse/telas/LoginTelas.kt
+++ app/src/main/java/com/example/diriodeclasse/telas/LoginTelas.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -21,7 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.diriodeclasse.R
@@ -66,8 +67,15 @@ fun LoginTelas(controledenavegacao: NavHostController) {
                 onValueChange = { senha = it },
                 label = { Text(text = "Senha") }
             )
+            Spacer(modifier = Modifier.height(25.dp))
+            Button (
+                onClick = { controledenavegacao.navigate("Telalistadealunos")
+                }
+            ){
+
+                Text(text = "Entrar")
+            }
 
         }
     }
 }
-@Preview(showBackground = true)
